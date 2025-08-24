862c5c3e5a6be5c6cf257854cd1191d529c13e15
bhaktiwalunj41 <walunjbhakti41@gmail.com>
2025-08-24 16:19:11 +0530

    Update todoReducer.js

--- src/reducer/todoReducer.js
+++ src/reducer/todoReducer.js
@@ -31,6 +31,15 @@ export function reducer(state, action){
         todos: state.todos.filter((todo) => todo.id !== action.payload),
       };
 
+    case "EDIT_TODO":
+      return {
+        ...state,
+        todos: state.todos.map((todo)=>
+          todo.id === action.payload.id
+            ? {...todo, ...action.payload.updates}
+            :todo
+          ),
+      }
 
             //case 'LOAD_TODOS':
 
